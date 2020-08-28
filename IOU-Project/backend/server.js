f6fd0ebc62be231ce5f04041f719570781dd41dc
Nam-Dang13581234 <nathandang73@gmail.com>
2020-08-28 10:48:57 +1000

    Add server code

--- IOU-Project/backend/server.js
+++ IOU-Project/backend/server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const port = 8080;
+
+// store the connection's string
 const connString = 'mongodb+srv://dbUser:notapassword@aipiou.d2nlx.mongodb.net/test?retryWrites=true&w=majority';
 
 const app = express();
 app.use(express.json()); // Make sure it comes back as json
 
+// connection to the cloud database
 mongoose.connect(connString, {
   useNewUrlParser: true,
   useUnifiedTopology: true 
 });
 
-app.listen(3003, () => { console.log('Server is running...') });
+const connection = mongoose.connection;
+// once connected execute the callback
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');  
+});
+
+app.listen(port, () => { 
+  console.log(`Server is running on port ${port}`) 
+});
