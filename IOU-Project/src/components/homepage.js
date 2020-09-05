6790fa7315d3d36f77b98a67308792879f96633b
Nam-Dang13581234 <nathandang73@gmail.com>
2020-09-05 23:24:35 +1000

    Show public requests

--- IOU-Project/src/components/homepage.js
+++ IOU-Project/src/components/homepage.js
@@ -9,12 +9,18 @@ export default class HomePage extends React.Component {
                 <section className='jumbotron text-centre'>
                     <div className='container'>
                         <h1 className='jumbotron-heading'>Public Requests</h1>
-                        <p className='lead text-muted'>
-                            This is a public request that is available to everyone.
-                            Anyone can view or browse any of these request favours.
-                            A leaderboard that showing the best user with the least amount of debt is available.
-                            leaderboard can be helpful bla bloh
-                        </p>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Username</th>
+                                    <th>Description</th>
+                                    <th>Favour</th>
+                                    <th>Rewards</th>
+                                    <th>When</th>
+                                </tr>
+                            </thead>
+                        </table>
+                        <br/>
                         <p>
                             <Link to='/leaderboard' className='btn btn-primary my-2'>Leaderboard</Link>
                             <Link to='/signup' className='btn btn-primary my-2'>Sign in to post a request</Link>
