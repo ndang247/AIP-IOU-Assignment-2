95d1bac12f6628e2acee4775176bad9879d7bf4f
Nam-Dang13581234 <nathandang73@gmail.com>
2020-09-06 17:51:57 +1000

    Fix signin rendering

--- IOU-Project/src/components/signin.js
+++ IOU-Project/src/components/signin.js
@@ -8,21 +8,23 @@ export default class SignIn extends React.Component {
             <body>
                 <main>
                     <div className='signin-box'>
-                        <form className='form-sigin'>
+                        <form className='form-signin'>  
                             <div>
                                 <h1 className='signinTitle'>Sign in</h1>
-                            </div>
-                            <div>
-                                <input type='email' id='inputEmail' className='form-control' placeholder='Email address' required='true' autoFocus='true'/>
+                            </div>             
+                            <div>  
+                                <input type='email' id='inputEmail' className='form-control' placeholder='Email adress' required='true' autoFocus='true'/>
                             </div>
                             <br/>
                             <div>
-                                <input type='password' id='inputPassword' className="form-control" placeholder="Password" required='true'/>
+                                <input type='password' id='inputPassword' className='form-control' placeholder='Password' required='true'/>
                             </div>
                             <br/>
                             <div className='checkbox mb-3'>
-                                <input type='checkbox' value='remember-me'/>
-                                <label>Remember me</label>
+                                <label>
+                                    <input type='checkbox' value='remember-me'/>
+                                    <label> Remember me </label>
+                                </label>
                             </div>
                             <br/>
                             <button className='btn btn-lg btn-primary btn-block' type='submit'>
