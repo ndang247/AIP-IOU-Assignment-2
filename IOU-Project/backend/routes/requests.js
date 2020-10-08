139072f5798808f3725dd5f5e9eb153a83babef7
Nam-Dang13581234 <nathandang73@gmail.com>
2020-10-08 18:13:04 +1100

    Add request api

--- IOU-Project/backend/routes/requests.js
+++ IOU-Project/backend/routes/requests.js
@@ -1,11 +1,80 @@
 const db = require('../models');
+const requestReward = require('../models/requestReward');
 module.exports = function (app, passport) {
     app.get('/api/all-requests', (req, res, next) => {
         // Get all requests that have not been accepted
-        db.sequelize.query('SELECT "Requests"."taskName", "Requests"."description", "RequestRewards"."quantity" FROM "Requests" INNER JOIN "RequestRewards" ON "Requests"."id" = "RequestRewards"."requestId"')
+        db.sequelize.query('SELECT "Requests"."id", "Requests"."taskName", "Requests"."description", "RequestRewards"."quantity" FROM "Requests" INNER JOIN "RequestRewards" ON "Requests"."id" = "RequestRewards"."requestId"')
         .then(data => res.json(data))
         .catch(err => res.status(400).json('Error:' + err));
     })
+
+    app.post('/api/update-requests/:id', (req, res, next) => {
+        // Update a request when user wants to edit
+        db.Request.findByPk(req.params.id)
+        .then(request => {
+            request.taskName = req.body.taskName;
+            request.description = req.body.description;
+        
+            request.save()
+            .then(() => res.json('Request updated!'))
+            .catch(err => res.status(400).json('Error ' + err));
+        })
+        .catch(err => res.status(400).json('Error:' + err));
+    })
+
+    app.delete('/api/delete-requests/:id', (req, res, next) => {
+        // Delete a request
+        db.Request.findByPk(req.params.id)
+        .then(request => {
+            request.destroy()
+            .then(() => res.json('Request deleted!'))
+            .catch(err => res.status(400).json('Error ' + err));
+        })
+        .catch(err => res.status(400).json('Error:' + err));
+    })
+
+    app.post('/api/add-requests', (req, res, next) => {
+        // Create a request
+
+        // Get the taskName, description, requesterName, reward and reward quantity
+        const taskName = req.body.taskName;
+        const description = req.body.description;
+        const requesterName = req.body.requesterName;
+        const rewardID = 1;
+        const rewardQuantity = Number(req.body.rewardQuantity);
+        const requesterID = 28;
+        db.Request.create({
+            taskName: taskName,
+            description: description,
+            requesterName: requesterName,
+        }).then(requestInstance => {
+            requestInstance.save()
+            db.RequestReward.create({
+                rewardId: rewardID,
+                quantity: rewardQuantity,
+                requesterId: requesterID,
+                requestId: requestInstance.id
+            }).then(requestRewardInstance => {
+                requestRewardInstance.save()
+            })
+        }).catch(err => res.status(400).json('Error ' + err));
+    })
+
+    app.post('/api/add-request-reward', (req, res, next) => {
+        // Add request reward
+        const rewardID = 1;
+        const rewardQuantity = Number(req.body.rewardQuantity);
+        const requesterID = req.user.id;
+        db.RequestReward.create({
+            rewardId: rewardID,
+            quantity: rewardQuantity,
+            requesterId: requesterID,
+            requestId: req.body.requestId
+        }).then(requestRewardInstance => {
+            requestRewardInstance.save();
+        }).catch(err => res.status(400).json('Error ' + err));
+    })
+
 
     // PRIVATE REQUESTS
     /*
