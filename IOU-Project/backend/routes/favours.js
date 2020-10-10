076ce4f513fa68ba82debfe974d8b466871fb13d
Nam-Dang13581234 <nathandang73@gmail.com>
2020-10-10 22:52:26 +1100

    Add favours api

--- IOU-Project/backend/routes/favours.js
+++ IOU-Project/backend/routes/favours.js
@@ -1,6 +1,17 @@
 const db = require('../models');
 
 module.exports = function (app, passport) {
+
+    app.get('/api/my-favours', (req, res, next) => {
+        // Get all my favours
+        db.sequelize.query('SELECT "Favours"."id", "Favours"."description", "FavourRewards"."quantity", "FavourRewards"."rewardId" FROM "Favours" INNER JOIN "FavourRewards" ON "Favours"."id" = "FavourRewards"."favourId"')
+        .then(data => {
+            res.json(data)
+        }
+        )
+        .catch(err => res.status(400).json('Error:' + err));
+    })
+
     app.post('/api/update-favours/:id', (req, res, next) => {
         // Update a favour when user wants to edit
         db.Favour.findByPk(req.params.id)
