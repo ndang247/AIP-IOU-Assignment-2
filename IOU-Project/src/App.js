3f628aaa14ef7c7a672756dd849b628572384edf
hlongg12 <hlongg12@gmail.com>
2020-10-17 14:30:55 +1100

    Modify app.js

--- IOU-Project/src/App.js
+++ IOU-Project/src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 // react router allows matching a specific route to a specific component for that page
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -11,23 +12,42 @@ import AddPublicRequest from './components/addpublicrequest';
 import AddViewFavour from './components/addandviewfavour';
 import AddViewDebt from './components/addandviewdebt';
 import "./Style.css";
-
+import axios from "axios"
+axios.defaults.withCredentials = true;
 // react will render the component based on the path/route specified
-function App() {
-  return (
-    <Router>
-        <Navbar/>
-        <br/>
-        <Route path='/' exact component={HomePage}/>
-        <Route path='/signup' component={SignUp}/>
-        <Route path='/signin' component={SignIn}/>
-        <Route path='/leaderboard' component={Leaderboard}/>
-        <Route path='/addpublicrequests' component={AddPublicRequest}/>
-        <Route path='/addandviewfavour' component={AddViewFavour}/>
-        <Route path='/addandviewdebt' component={AddViewDebt}/>
-        <Footer/>
-    </Router>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: false };
+  }
+  setLoggedIn() {
+    this.setState({ isLoggedIn: true });
+  }
+  setLoggedOut() {
+    this.setState({ isLoggedIn: false });
+  }
+  render() {
+    return (
+      <Router>
+        <Navbar isLoggedIn={this.state.isLoggedIn} />
+        <br />
+        <Route path='/' exact component={HomePage} />
+        <Route path='/signup' component={(props) =>
+          <SignUp {...props} abc={this.state.isLoggedIn} setLoggedIn={() => this.setLoggedIn()} setLoggedOut={() => this.setLoggedOut()} />
+        } />
+        <Route path='/signin' component={(props) =>
+          <SignIn {...props} isLoggedn={this.state.isLoggedIn} setLoggedIn={() => this.setLoggedIn()} setLoggedOut={() => this.setLoggedOut()} />
+        } />
+        <Route path='/leaderboard' component={Leaderboard} />
+        <Route path='/addpublicrequests' component={AddPublicRequest} />
+        <Route path='/addandviewfavour' component={AddViewFavour} />
+        <Route path='/addandviewdebt' component={AddViewDebt} />
+        <Footer />
+      </Router>
+    );
+  }
 }
-
 export default App;
+
+
+
